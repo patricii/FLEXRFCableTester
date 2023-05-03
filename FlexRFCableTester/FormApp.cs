6c8f7f1fad05b86a5c0edb6aa698b4c894ffb712
jagapatr <jagapatr@motorola.com>
2023-05-03 12:56:15 -0300

    Added Update method

--- FlexRFCableTester/FormApp.cs
+++ FlexRFCableTester/FormApp.cs
@@ -180,8 +180,9 @@ namespace FlexRFCableTester
                 dataGridViewMeasureTable.Rows[count].Cells[4].Value = hiLimit;
                 dataGridViewMeasureTable.Rows[count].Cells[5].Value = calFactor;
                 dataGridViewMeasureTable.Rows[count].Cells[6].Value = passFail;
-                dataGridViewMeasureTable.Rows[count].Cells[7].Value = testTime;
+                dataGridViewMeasureTable.Rows[count].Cells[7].Value = testTime;                
                 Application.DoEvents();
+                Update();
             }
             catch (Exception ex)
             {
